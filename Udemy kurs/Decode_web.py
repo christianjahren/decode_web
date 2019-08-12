320a36b98e4fa2b1b413629920e48eec3b3a8a65
Christian Jahren <christian_jahren@hotmail.com>
2019-08-12 10:47:14 +0200

    Update

--- Udemy kurs/Decode_web.py
+++ Udemy kurs/Decode_web.py
@@ -1,8 +1,16 @@
+############# E17 Decode a web page ############
 from bs4 import BeautifulSoup
+import requests
+def decode(url):
+    # requests the website
+    r = requests.get(url)
+    # store the text from the website as a variable to make it easier to read
+    r_html = r.text
 
+    # run the text through BeautifulSoup with the "html.parser", as well as store this in a variable
+    soup = BeautifulSoup(r_html, 'html.parser')
 
-def source_code(args):
-    pass
-
-
-soup = BeautifulSoup(source_code, 'html.parser')
+    #
+    for item in soup.find_all("h3"):
+        print(item.text)
+decode("https://www.vg.no/")
